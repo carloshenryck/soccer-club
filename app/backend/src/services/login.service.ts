d3ff2a52e07f231e9fc82b47aa36c9a40380008c
Henryck <ryck302@gmail.com>
2022-11-27 00:14:14 -0300

    requisito 09 e 11

--- app/backend/src/services/login.service.ts
+++ app/backend/src/services/login.service.ts
@@ -1,10 +1,17 @@
+import { compare } from 'bcryptjs';
+import { Unauthorized } from '../@types/errors';
 import { ILoginData, IUserDataFromDatabase } from '../@types/ILogin';
 import User from '../database/models/User';
 import * as jwtUtils from '../utils/jwt.util';
 
 export default class LoginService {
-  static async validateLogin({ email }: ILoginData): Promise<string> {
+  static async validateLogin({ email, password }: ILoginData): Promise<string> {
     const user = await User.findOne({ where: { email } });
+
+    if (!user || !await compare(password, user.password)) {
+      throw new Unauthorized('Incorrect email or password');
+    }
+
     const { password: ps, ...userWithoutPassword } = user?.dataValues as IUserDataFromDatabase;
     const token = jwtUtils.createToken(userWithoutPassword);
     return token;
