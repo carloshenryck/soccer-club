770c5449e5345eae93ea88fe3042b85705a103c7
Henryck <ryck302@gmail.com>
2022-12-14 21:33:15 -0300

    requisito 34 e 35

--- app/backend/src/middlewares/matchValidator.ts
+++ app/backend/src/middlewares/matchValidator.ts
@@ -4,7 +4,8 @@ import { BadRequest, UnprocessableEntity } from '../@types/errors';
 const validateMatch = (req: Request, _res: Response, next: NextFunction) => {
   const { homeTeam, awayTeam, homeTeamGoals, awayTeamGoals } = req.body;
 
-  if (!homeTeam || !awayTeam || !homeTeamGoals || !awayTeamGoals) {
+  if (!homeTeam || !awayTeam || homeTeamGoals === undefined || awayTeamGoals === undefined) {
+    console.log(homeTeam, awayTeam, homeTeamGoals, awayTeamGoals);
     throw new BadRequest('All fields must be filled');
   }
 
